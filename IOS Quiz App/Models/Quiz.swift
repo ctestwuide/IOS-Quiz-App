766c3659ef72399945150d61980528d116ce795f
ctestwuide <cdt56@cornell.edu>
2024-03-13 00:19:15 -0400

    Quiz model created

---
+++ IOS Quiz App/Models/Quiz.swift
@@ -0,0 +1,14 @@
+//
+//  Quiz.swift
+//  IOS Quiz App
+//
+//  Created by Chase Testwuide on 3/12/24.
+//
+
+import Foundation
+
+struct Quiz {
+    var type: QuizType
+    var questions: [Question]
+    var score: Int = 0
+}
