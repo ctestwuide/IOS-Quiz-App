cf157c6b702759e246fb9c5c4b7b9d03de7b5d81
ctestwuide <cdt56@cornell.edu>
2024-03-12 22:57:03 -0400

    Initial commit

---
+++ IOS Quiz App/IOS_Quiz_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_Quiz_AppApp.swift
+//  IOS Quiz App
+//
+//  Created by Chase Testwuide on 3/12/24.
+//
+
+import SwiftUI
+
+@main
+struct IOS_Quiz_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
