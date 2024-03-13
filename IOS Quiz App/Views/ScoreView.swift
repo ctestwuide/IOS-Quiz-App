60965462385def1626ec72378c2295b45f0802fa
ctestwuide <cdt56@cornell.edu>
2024-03-13 00:34:37 -0400

    Added View files

---
+++ IOS Quiz App/Views/ScoreView.swift
@@ -0,0 +1,18 @@
+//
+//  ScoreView.swift
+//  IOS Quiz App
+//
+//  Created by Chase Testwuide on 3/13/24.
+//
+
+import SwiftUI
+
+struct ScoreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScoreView()
+}
---
+++ IOS Quiz App/Views/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  IOS Quiz App
+//
+//  Created by Chase Testwuide on 3/13/24.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
