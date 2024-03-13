a1fc2cfac038e197c02aa3c7b303e76c65966a43
ctestwuide <cdt56@cornell.edu>
2024-03-13 00:19:33 -0400

    QuizType model created for multiple quiz genres

---
+++ IOS Quiz App/Models/QuizType.swift
@@ -0,0 +1,16 @@
+//
+//  QuizType.swift
+//  IOS Quiz App
+//
+//  Created by Chase Testwuide on 3/12/24.
+//
+
+import Foundation
+
+enum QuizType: CaseIterable {
+    case pokemon
+    case greekMythology
+    case office
+    
+    
+}
